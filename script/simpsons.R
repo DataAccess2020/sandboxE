226bb359a8ddd991a4fcadca95e1392d9d1ec494
ERICA RAVARELLI <ravarellierica@gmail.com>
2022-02-10 16:01:17 +0100

    print titles

---
+++ script/simpsons.R
@@ -0,0 +1,16 @@
+setwd("/Users/ravar/OneDrive/Documents/uni/Magistrale/primo anno/secondo trimestre/data access and regulation/De Angelis")
+
+library(tidyverse)
+load("s3-simpsons.Rdata")
+
+view("s3-simpsons.Rdata")
+str(simpsons[[1]])
+
+
+simpsons[["1"]][["title"]]
+simpsons[["2"]][["title"]]
+
+#capitolo 21 iteration
+
+
+
