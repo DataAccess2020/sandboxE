9800434d175583979f65583a1e7775cb2c64033b
ERICA RAVARELLI <ravarellierica@gmail.com>
2022-02-10 16:01:47 +0100

    print titles

--- script/simpsons.R
+++ script/simpsons.R
@@ -1,4 +1,4 @@
-setwd("/Users/ravar/OneDrive/Documents/uni/Magistrale/primo anno/secondo trimestre/data access and regulation/De Angelis")
+setwd("/Users/ravar/OneDrive/Documents/uni/Magistrale/primo anno/secondo trimestre/data access and regulation/De Angelis/sandboxE/script")
 
 library(tidyverse)
 load("s3-simpsons.Rdata")
@@ -6,9 +6,15 @@ load("s3-simpsons.Rdata")
 view("s3-simpsons.Rdata")
 str(simpsons[[1]])
 
+length(simpsons)
 
+seq_along(simpsons) #vettore
 simpsons[["1"]][["title"]]
 simpsons[["2"]][["title"]]
+
+for (i in seq_along (simpsons)) {
+cat (simpsons[[i]][["title"]], "\n")
+} 
 
 #capitolo 21 iteration
 
